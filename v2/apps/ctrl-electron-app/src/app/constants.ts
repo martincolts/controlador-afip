d4954cb1e5d4b7e9764a05882a65bb0fa6dc0fb8
Tincho <martinlopez89.quequen@gmail.com>
2023-10-19 00:26:26 -0300

    added v2

---
+++ v2/apps/ctrl-electron-app/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'ctrl-react-app'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'ctrl-electron-app';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
